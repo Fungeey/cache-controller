ecfa28a2ecbbaebdf8a67f2f965e77c2ce243d55
Stanley Tan <stanleytan2002@live.ca>
2024-10-09 13:36:09 -0400

    change

--- CacheController.vhd
+++ CacheController.vhd
@@ -6,16 +6,5 @@ use IEEE.STD_LOGIC_UNSIGNED.ALL;
 entity CacheController is
 	Port    (   clk 		: in    STD_LOGIC;
 				reset		: in    STD_LOGIC;
-				addr_in		: in 	STD_LOGIC_VECTOR(15 downto 0);
-                cs          : in    STD_LOGIC;
-
-
-                memstrb     : out   STD_LOGIC;
-                rdy         : out   STD_LOGIC;
-                wen         : out   STD_LOGIC;
-
-                din_mux     : out   STD_LOGIC;
-                dout_mux    : out   STD_LOGIC;
-
-                );
+				
 end CacheController;
