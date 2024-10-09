453d13958c6efcced4047dd10cd3d94eb73d017f
Stanley Tan <stanleytan2002@live.ca>
2024-10-09 13:44:20 -0400

    new file + some changes to cache.vhd

--- cache.vhd
+++ cache.vhd
@@ -3,19 +3,37 @@ use IEEE.STD_LOGIC_1164.ALL;
 use IEEE.STD_LOGIC_ARITH.ALL;
 use IEEE.STD_LOGIC_UNSIGNED.ALL;
 
-entity CacheController is
-	Port    (   clk 		: in    STD_LOGIC;
+entity cache is -- the ports of the cache controller
+	Port    (   
+                --inputs
+                clk 		: in    STD_LOGIC;
 				reset		: in    STD_LOGIC;
 				addr_in		: in 	STD_LOGIC_VECTOR(15 downto 0);
                 cs          : in    STD_LOGIC;
 
-
+                --outputs
                 memstrb     : out   STD_LOGIC;
                 rdy         : out   STD_LOGIC;
                 wen         : out   STD_LOGIC;
 
+                -- to SRAM
                 din_mux     : out   STD_LOGIC;
                 dout_mux    : out   STD_LOGIC;
 
                 );
-end CacheController;
+end cache;
+
+architecture Behavioral of cache is
+
+--components and signals goes here
+
+
+
+
+
+begin
+
+    --port map?
+    
+    process (clk)
+    begin
